abfcb132c38ba75ae87ec018d4040fd239026fb6
huulamnguyen <liamnguyen.swe@gmail.com>
2021-12-19 22:25:06 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.querySelector('#create-task-form')
+
+  newTaskForm.addEventListener('submit', createNewTask);
+
+  function createNewTask(e) {
+    e.preventDefault();
+
+    const newTaskDescription = document.getElementById('new-task-description');
+
+    const newTask = document.createElement('li');
+    const removeBtn = document.createElement('button')
+    removeBtn.addEventListener('click', removeTask);
+    removeBtn.textContent = "x";
+    newTask.textContent = `${newTaskDescription.value} `;
+
+    newTask.appendChild(removeBtn)
+
+    appendNewTask(newTask);
+    e.target.reset();
+  }
+
+  function removeTask(e) {
+    e.target.parentNode.remove()
+  }
+
+  function appendNewTask(newTask) {
+    document.getElementById('tasks').appendChild(newTask);
+  }
+
 });
